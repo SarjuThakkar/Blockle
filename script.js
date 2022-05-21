89843e54994163dd02ac72d35d5c12ad7ba6887f
Sarju Thakkar <47423423+SarjuThakkar@users.noreply.github.com>
2022-05-20 20:05:16 -0400

    day 14 board

--- script.js
+++ script.js
@@ -35,74 +35,68 @@ var grid = [
 // First block is main red block
 // Orientation 0 for horizontal and 1 for vertical
 const data = {
-    day: 13,
-    moves: 15,
+    day: 14,
+    moves: 17,
     blocks: [
         {
             orientation: 0,
-            x: 0,
+            x: 2,
             y: 2,
             length: 2
         },
         {
             orientation: 0,
             x: 0,
-            y: 0,
-            length: 3
-        },
-        {
-            orientation: 0,
-            x: 0,
             y: 1,
             length: 2
         },
         {
             orientation: 1,
-            x: 4,
-            y: 0,
-            length: 3
-        },
-        {
-            orientation: 1,
-            x: 5,
-            y: 1,
-            length: 3
-        },
-        {
-            orientation: 1,
             x: 0,
-            y: 3,
-            length: 3
+            y: 2,
+            length: 2
         },
         {
-            orientation: 0,
+            orientation: 1,
             x: 1,
-            y: 3,
+            y: 2,
             length: 2
         },
         {
-            orientation: 0,
-            x: 3,
+            orientation: 1,
+            x: 4,
+            y: 0,
+            length: 3
+        },
+        {
+            orientation: 1,
+            x: 2,
             y: 3,
             length: 2
         },
         {
             orientation: 1,
+            x: 1,
+            y: 4,
+            length: 2
+        },
+        {
+            orientation: 0,
+            x: 3,
+            y: 3,
+            length: 2
+        },
+        {
+            orientation: 0,
             x: 3,
             y: 4,
             length: 2
         },
         {
             orientation: 0,
-            x: 4,
-            y: 4,
-            length: 2
-        },
-        {
-            orientation: 0,
-            x: 4,
+            x: 2,
             y: 5,
-            length: 2
+            length: 3
         }
     ]
 }
